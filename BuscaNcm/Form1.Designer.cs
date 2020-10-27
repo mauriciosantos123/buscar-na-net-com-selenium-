e231e913066a0d27200495d4dab321878e8b5595
mauriciosantos123 <50496179+mauriciosantos123@users.noreply.github.com>
2020-10-27 17:30:38 -0300

    primeiro commit

---
+++ BuscaNcm/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace BuscaNcm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtNcm = new System.Windows.Forms.TextBox();
+            this.txtncm2 = new System.Windows.Forms.TextBox();
+            this.boxEstado = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dataGridViewResultados = new System.Windows.Forms.DataGridView();
+            this.result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCest1 = new System.Windows.Forms.MaskedTextBox();
+            this.txtCest = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNcm
+            // 
+            this.txtNcm.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtNcm.Location = new System.Drawing.Point(13, 99);
+            this.txtNcm.Name = "txtNcm";
+            this.txtNcm.Size = new System.Drawing.Size(111, 20);
+            this.txtNcm.TabIndex = 0;
+            this.txtNcm.Text = "4 N° ncm : 2106";
+            this.txtNcm.Enter += new System.EventHandler(this.ncmEnter);
+            // 
+            // txtncm2
+            // 
+            this.txtncm2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtncm2.Location = new System.Drawing.Point(146, 99);
+            this.txtncm2.Name = "txtncm2";
+            this.txtncm2.Size = new System.Drawing.Size(35, 20);
+            this.txtncm2.TabIndex = 1;
+            this.txtncm2.Text = "00";
+            this.txtncm2.Enter += new System.EventHandler(this.ncm2Enter);
+            // 
+            // boxEstado
+            // 
+            this.boxEstado.FormattingEnabled = true;
+            this.boxEstado.Location = new System.Drawing.Point(348, 98);
+            this.boxEstado.Name = "boxEstado";
+            this.boxEstado.Size = new System.Drawing.Size(121, 21);
+            this.boxEstado.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnBuscar.Location = new System.Drawing.Point(475, 98);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(117, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // dataGridViewResultados
+            // 
+            this.dataGridViewResultados.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridViewResultados.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewResultados.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridViewResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResultados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.result,
+            this.result2,
+            this.result3,
+            this.result4,
+            this.result5,
+            this.result6,
+            this.result7});
+            this.dataGridViewResultados.Location = new System.Drawing.Point(13, 160);
+            this.dataGridViewResultados.Name = "dataGridViewResultados";
+            this.dataGridViewResultados.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewResultados.Size = new System.Drawing.Size(732, 264);
+            this.dataGridViewResultados.TabIndex = 5;
+            // 
+            // result
+            // 
+            this.result.HeaderText = "1";
+            this.result.Name = "result";
+            // 
+            // result2
+            // 
+            this.result2.HeaderText = "2";
+            this.result2.Name = "result2";
+            // 
+            // result3
+            // 
+            this.result3.HeaderText = "3";
+            this.result3.Name = "result3";
+            // 
+            // result4
+            // 
+            this.result4.HeaderText = "4";
+            this.result4.Name = "result4";
+            // 
+            // result5
+            // 
+            this.result5.HeaderText = "5";
+            this.result5.Name = "result5";
+            // 
+            // result6
+            // 
+            this.result6.HeaderText = "6";
+            this.result6.Name = "result6";
+            // 
+            // result7
+            // 
+            this.result7.HeaderText = "7";
+            this.result7.Name = "result7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NCM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cest";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Estados";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "os 4n° Ncm. EX :2106";
+            // 
+            // txtCest1
+            // 
+            this.txtCest1.Location = new System.Drawing.Point(217, 98);
+            this.txtCest1.Mask = "00,000,00";
+            this.txtCest1.Name = "txtCest1";
+            this.txtCest1.Size = new System.Drawing.Size(111, 20);
+            this.txtCest1.TabIndex = 12;
+            // 
+            // txtCest
+            // 
+            this.txtCest.Location = new System.Drawing.Point(634, 430);
+            this.txtCest.Name = "txtCest";
+            this.txtCest.Size = new System.Drawing.Size(111, 20);
+            this.txtCest.TabIndex = 2;
+            this.txtCest.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.txtCest1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewResultados);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.boxEstado);
+            this.Controls.Add(this.txtCest);
+            this.Controls.Add(this.txtncm2);
+            this.Controls.Add(this.txtNcm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNcm;
+        private System.Windows.Forms.TextBox txtncm2;
+        private System.Windows.Forms.ComboBox boxEstado;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dataGridViewResultados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn result7;
+        private System.Windows.Forms.MaskedTextBox txtCest1;
+        private System.Windows.Forms.TextBox txtCest;
+    }
+}
+
---
+++ BuscaNcm/Form1.cs
@@ -0,0 +1,299 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+
+namespace BuscaNcm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            #region Estados
+
+
+            // box dos estados 
+
+            boxEstado.Items.Add("AC");
+            boxEstado.Items.Add("AL");
+            boxEstado.Items.Add("AP");
+            boxEstado.Items.Add("AM");
+            boxEstado.Items.Add("BA");
+            boxEstado.Items.Add("CE");
+            boxEstado.Items.Add("DF");
+            boxEstado.Items.Add("ES");
+            boxEstado.Items.Add("GO");
+            boxEstado.Items.Add("MA");
+            boxEstado.Items.Add("MT");
+            boxEstado.Items.Add("MS");
+            boxEstado.Items.Add("MG");
+            boxEstado.Items.Add("PA");
+            boxEstado.Items.Add("PB");
+            boxEstado.Items.Add("PR");
+            boxEstado.Items.Add("PE");
+            boxEstado.Items.Add("PI");
+            boxEstado.Items.Add("RJ");
+            boxEstado.Items.Add("RN");
+            boxEstado.Items.Add("RS");
+            boxEstado.Items.Add("RO");
+            boxEstado.Items.Add("RR");
+            boxEstado.Items.Add("SC");
+            boxEstado.Items.Add("SP");
+            boxEstado.Items.Add("SE");
+            boxEstado.Items.Add("TO");
+            // Começar pelo primeiro estado 
+            boxEstado.SelectedIndex = 0;
+            #endregion
+
+    
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            MaskedTextBox mask = new MaskedTextBox();
+
+
+
+            int ncm;
+            if (!int.TryParse(txtNcm.Text, out ncm) && ncm == null)
+            {
+                MessageBox.Show("somente numeros são aceitos no campo NCM");
+                return;
+            }
+            int i;
+            if (!int.TryParse(txtncm2.Text, out i) && i == null)
+            {
+                MessageBox.Show("somente numeros são aceitos para complementar ncm");
+                return;
+            }
+            dataGridViewResultados.Rows.Clear();
+            while (boxEstado.SelectedIndex <= 30 )
+            {
+               
+                try
+                {
+
+                    //abri o browser com selenium _______________________________________________________________
+                    ChromeOptions options = new ChromeOptions();
+                    options.AddArgument("--headless");
+                    ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+
+
+                    //adicionar o service e option para não mostra o prompt e nem browser _____________________________________________________
+                    ChromeDriver driver = new ChromeDriver(service,options);
+
+                    driver.Navigate().GoToUrl("http://www.econeteditora.com.br/icms_st/index.php?form%5Buf%5D=" + boxEstado.Text + "&form%5Bncm%5D=" + txtNcm.Text + "&form%5Bcest%5D=" + txtCest1.Text + "&form%5Bpalavra%5D=&acao=Buscar");
+
+                    var clicar = driver.FindElement(By.TagName("input"));
+                    clicar.Click();
+                    //LOGIM_______________________________________________________________________________
+                    string login = "onu16035";
+                    string senha = "gomide2";
+
+                    var userInput = driver.FindElementByName("Log");
+                    userInput.SendKeys(login);
+
+                    var passwordInput = driver.FindElementByName("Sen");
+                    passwordInput.SendKeys(senha + Keys.Enter);
+
+                    driver.Navigate().GoToUrl("http://www.econeteditora.com.br/icms_st/index.php?form%5Buf%5D=" + boxEstado.Text + "&form%5Bncm%5D=" + txtNcm.Text + "&form%5Bcest%5D=" + txtCest1.Text + "&form%5Bpalavra%5D=&acao=Buscar");
+
+
+                 
+
+
+                    //A BUSCA  NCM ____________________________________________________________________________
+                    if (i == 00 || i == null)
+                    {
+                        var Primerabusca = driver.FindElementByTagName("input");
+                        Primerabusca.Click();
+                    }
+                    else if (i > 00 && i <= 90)
+                    {
+                        var segundabusca = driver.FindElementByXPath("/html/body/table[3]/tbody/tr[3]/td[3]/input[1]");
+                        segundabusca.Click();
+
+                    }
+                    else if (i > 90 && i <= 9010)
+                    {
+                        var terceirabusca = driver.FindElementByXPath("/html/body/table[3]/tbody/tr[7]/td[3]/input[1]");
+                        terceirabusca.Click();
+                    }
+                    else
+                    {
+                        var quartabusca = driver.FindElementByXPath("/html/body/table[3]/tbody/tr[8]/td[3]/input[1]");
+                        quartabusca.Click();
+                    }
+
+
+                    #region PEGAR OS DADOS DA PAGINA ATUAL PELO XPATH tit= titulo
+
+                    //PEGAR OS DADOS DA PAGINA ATUAL PELO XPATH tit= titulo _________________________________________________________
+
+                    var tit = "";
+                    try { tit = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[1]").Text; } catch { tit = "não existe"; }
+                    //-----------
+                    var mvaO = "";
+                    try { mvaO = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[1]").Text; } catch { mvaO = "não existe"; }
+                    //------------
+
+                    var tit1 = "";
+                    try { tit1 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[2]").Text; } catch { tit1 = "não existe"; }
+                    //--------
+                    var mva4 = "";
+                    try { mva4 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[2]").Text; } catch { mva4 = "não existe"; }
+                    //--------  
+                    var tit2 = "";
+                    try { tit2 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[3]").Text; } catch { tit2 = "não existe"; }
+                    //------------
+                    var mva7 = "";
+                    try { mva7 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[3]").Text; } catch { mva7 = "não existe"; }
+                    //----------  
+                    var tit3 = "";
+                    try { tit3 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[4]").Text; } catch { tit3 = "não existe"; }
+                    //--------
+                    var mva12 = "";
+                    try { mva12 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[4]").Text; } catch { mva12 = "não existe"; }
+                    //-------
+
+                    var tit4 = "";
+                    try { tit4 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[5]").Text; } catch { tit4 = "não existe"; }
+
+                    //-------    
+                    var aliq = "";
+                    try { aliq = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[5]").Text; } catch { aliq = "não existe"; }
+
+                    //-----
+
+                    var tit5 = "";
+                    try { tit5 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[1]/td[6]").Text; } catch { tit5 = "não existe"; }
+                    //----------
+                    var fecp = "";
+                    try { fecp = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[2]/td[6]").Text; } catch { fecp = "não existe"; }
+
+
+                    //CASO HOUVER UMA SEGUNDA LINHA ADICIONAR AQUI______________________________________________________________________ 
+
+                    var T1 = "";
+                    try { T1 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[1]").Text; } catch { T1 = ""; }
+
+                    var T2 = "";
+                    try { T2 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[2]").Text; } catch { T2 = ""; }
+
+                    var T3 = "";
+                    try { T3 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[3]").Text; } catch { T3 = ""; }
+
+                    var T4 = "";
+                    try { T4 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[4]").Text; } catch { T4 = ""; }
+
+                    var T5 = "";
+                    try { T5 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[5]").Text; } catch { T5 = ""; }
+
+                    var T6 = "";
+                    try { T6 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[3]/td[6]").Text; } catch { T6 = ""; }
+                    // 3 LINHA TABELA SE EXISTIR ___________________________________________________________________________
+
+                    var C1 = "";
+                    try { C1 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[1]").Text; } catch { C1 = ""; }
+
+                    var C2 = "";
+                    try { C2 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[2]").Text; } catch { C2 = ""; }
+
+                    var C3 = "";
+                    try { C3 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[3]").Text; } catch { C3 = ""; }
+
+                    var C4 = "";
+                    try { C4 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[4]").Text; } catch { C4 = ""; }
+
+                    var C5 = "";
+                    try { C5 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[5]").Text; } catch { C5 = ""; }
+
+                    var C6 = "";
+                    try { C6 = driver.FindElementByXPath("//*[@id=\"TabbedPanels1\"]/div/div[1]/table[3]/tbody/tr[4]/td[6]").Text; } catch { C6 = ""; }
+
+
+                    #endregion
+
+
+                    //Limpar e Adicionar valores na tabela__________________________________________________________________________________________________
+                    //dataGridViewResultados.Rows.Clear();
+
+                    dataGridViewResultados.Rows.Add(boxEstado.Text, tit, tit1, tit2, tit3, tit4, tit5);
+                  //  dataGridViewResultados.DefaultCellStyle.BackColor = Color.Cyan;
+
+                    dataGridViewResultados.Rows.Add(boxEstado.Text, mvaO, mva4, mva7, mva12, aliq, fecp);
+
+                    dataGridViewResultados.Rows.Add(boxEstado.Text, T1, T2, T3, T4, T5, T6);
+
+                    dataGridViewResultados.Rows.Add(boxEstado.Text, C1, C2, C3, C4, C5, C6);
+
+                    
+                    driver.Close();
+                    driver.Quit();
+
+                }
+                catch
+                {
+                    MessageBox.Show("É preciso colocar NCM ou CEst " );
+                }
+
+
+                if ((boxEstado.SelectedIndex % 3) == 0 || boxEstado.Text == "TO") { break; return; }
+                else
+                {
+
+
+                    boxEstado.SelectedIndex++;
+                }
+
+
+            }
+            // limpar a caixa 
+            txtCest1.Clear();
+            txtNcm.Clear();
+            txtncm2.Clear();
+            txtncm2.Text = "00";
+            MessageBox.Show("Pesquisa concluida");
+       
+        }
+        #region Marca d'água
+
+        private void ncmEnter(object sender, EventArgs e)
+        {
+            if (txtNcm.Text == "4 N° ncm : 2106")
+            {
+                txtNcm.Text = "";
+
+                txtNcm.ForeColor = Color.Black;
+            }
+        }
+
+        private void ncm2Enter(object sender, EventArgs e)
+        {
+            if (txtncm2.Text == "00")
+            {
+                txtncm2.Text = "";
+
+                txtncm2.ForeColor = Color.Black;
+            }
+        }
+        #endregion
+
+    }
+}
